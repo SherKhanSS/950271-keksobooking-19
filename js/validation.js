21dc7ab9e6c0d205a3f0f6ff2d7c1250b9cbaf4a
SherKhan <gurovg@gmail.com>
2020-02-12 23:14:54 +0300

    исправляет замнчания

--- js/validation.js
+++ js/validation.js
@@ -3,9 +3,6 @@
 (function () {
 
   var MAX_PRICE = 1000000;
-  var MIN_FLAT = 1000;
-  var MIN_HOUSE = 5000;
-  var MIN_PALACE = 10000;
   var minPricePlaceholder = {
     'bungalo': 0,
     'flat': 1000,
@@ -16,41 +13,48 @@
   var noticeFormElement = document.querySelector('.notice');
   var noticePriceElement = noticeFormElement.querySelector('#price');
   var noticeTypeElement = noticeFormElement.querySelector('#type');
+  var noticeTimeElements = noticeFormElement.querySelector('.ad-form__element--time');
+  var noticeTimeInElements = noticeTimeElements.querySelector('#timein');
+  var noticeTimeOutElements = noticeTimeElements.querySelector('#timeout');
 
   noticePriceElement.addEventListener('input', function (evt) {
     var target = evt.target;
-    if (target.value > MAX_PRICE) {
+    if (Number(target.value) > MAX_PRICE) {
       target.setCustomValidity(
-          'А ты хорош, начинающий бизнесмен. Пыл поумерь до ' +
+          'А ты хорош, начинающий бизнесмен! Пыл поумерь до ' +
         MAX_PRICE +
         ' деревянных.'
       );
-    } else if (noticeTypeElement.value === 'flat' && target.value < MIN_FLAT) {
+    } else if (Number(target.value) < minPricePlaceholder[noticeTypeElement.value]) {
       target.setCustomValidity(
-          'У тебя там что, комната в сарае? Добропорядочные люди меньше ' +
-        MIN_FLAT +
-        ' деревянных не берут.'
-      );
-    } else if (noticeTypeElement.value === 'house' && target.value < MIN_HOUSE) {
-      target.setCustomValidity(
-          'Совсем все плохо? Меньше ' +
-        MIN_HOUSE +
-        ' деревянных указать нельзя.'
-      );
-    } else if (noticeTypeElement.value === 'palace' && target.value < MIN_PALACE) {
-      target.setCustomValidity(
-          'Тебе никто не поверит! Нынче дворцы меньше чем за ' +
-        MIN_PALACE +
-        ' деревянных не стоят.'
+          'Дешевишь, бро! Меньше ' +
+        minPricePlaceholder[noticeTypeElement.value] +
+        ' низя.'
       );
     } else {
       target.setCustomValidity('');
     }
   });
-  // тут, похоже, можно через обьект свести условия к одному, но пока реализовать не смог
 
-  noticeTypeElement.addEventListener('click', function (evt) {
+  noticeTypeElement.addEventListener('change', function (evt) {
     noticePriceElement.placeholder = minPricePlaceholder[evt.target.value];
   });
+
+  noticeTimeElements.addEventListener('change', function (evt) {
+    if (evt.target.id === 'timein') {
+      noticeTimeOutElements.selectedIndex = noticeTimeInElements.selectedIndex;
+    } else {
+      noticeTimeInElements.selectedIndex = noticeTimeOutElements.selectedIndex;
+    }
+  });
+
+  var noticeRoomNumberElements = noticeFormElement.querySelector('#room_number');
+  var noticeCapacityElements = noticeFormElement.querySelector('#capacity');
+  var roomСapacity = {
+    1: [1],
+    2: [1, 2],
+    3: [1, 2, 3],
+    100: [0],
+  };
 
 })();
